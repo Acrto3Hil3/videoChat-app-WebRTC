5e43ca7dbec1371da057854d0dcabe5398c8cb73
Acrto3Hil3 <subhashyadav98146@gmail.com>
2024-11-25 00:51:04 +0530

    tailwind functionalites added

---
+++ client/tailwind.config.js
@@ -0,0 +1,21 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
